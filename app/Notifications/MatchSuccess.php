200a8c411228a8fbb7ed6c194a235ba518af026d
Edu Calero (DigitalOcean Server) <educalerorovira@gmail.com>
2019-07-05 13:34:32 +0000

    Se añaden las modificaiones para implementar las APN

---
+++ app/Notifications/MatchSuccess.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use SemyonChetvertnyh\ApnNotificationChannel\ApnMessage;
+
+class MatchSuccess extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['apn'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the APN representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return ApnMessage
+     */
+    public function toApn($notifiable)
+    {
+        return ApnMessage::create()
+            ->badge(1)
+            ->title('IFLET.TECH')
+            ->body("Próximamente guia completa de APN!");
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
